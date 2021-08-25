690b4fbf823a436497768f37c64e48fd8529c9c4
Maria Rushkova <maryrushkova@gmail.com>
2021-08-25 12:32:03 +0200

    Add styles & disconnect

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -27,12 +27,17 @@ const config = {
 export const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
-     }
+  }
 
   body, html, #root {
     margin: 0;
     width: 100%;
     height: 100%;
+  }
+
+  html {
+    font-family: Inter;
+    font-style: normal;
   }
 `
 
