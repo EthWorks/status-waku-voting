f4084376f121fedf8a7c231ab1a9e8608748b8a3
Maria Rushkova <maryrushkova@gmail.com>
2021-09-02 14:28:18 +0200

    Add proposal TopBar

--- packages/proposal-page/src/index.tsx
+++ packages/proposal-page/src/index.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { useTest } from '@status-waku-voting/proposal-hooks'
 import { Proposal } from '@status-waku-voting/proposal-components'
+import { TopBar } from '@status-waku-voting/react-components'
+import votingIcon from './assets/images/voting.svg'
+import styled from 'styled-components'
 
 export function ProposalPage() {
   useTest()
   return (
-    <div>
+    <Wrapper>
+      <TopBar logo={votingIcon} title={'Proposals Dapp'} color={'#5D7BE2'} />
       <Proposal />
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`
