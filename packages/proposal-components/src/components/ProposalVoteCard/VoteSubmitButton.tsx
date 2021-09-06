42765722459e6e90cd23dd0c7cb8ac0a1d6fb086
Maria Rushkova <maryrushkova@gmail.com>
2021-09-06 17:26:15 +0200

    Add commas to saving button

--- packages/proposal-components/src/components/ProposalVoteCard/VoteSubmitButton.tsx
+++ packages/proposal-components/src/components/ProposalVoteCard/VoteSubmitButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { addCommas } from '../../helpers/addCommas'
 import { VoteSendingBtn } from '../Buttons'
 
 interface VoteSubmitButtonProps {
@@ -8,7 +9,7 @@ interface VoteSubmitButtonProps {
 
 export function VoteSubmitButton({ votes, disabled }: VoteSubmitButtonProps) {
   if (votes > 0) {
-    return <VoteSendingBtn disabled={disabled}> {votes} votes need saving</VoteSendingBtn>
+    return <VoteSendingBtn disabled={disabled}> {addCommas(votes)} votes need saving</VoteSendingBtn>
   }
   return null
 }
