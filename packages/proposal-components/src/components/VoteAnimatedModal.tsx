60c5b99877a64e6fd12f828997335a8fbc34579a
Maria Rushkova <maryrushkova@gmail.com>
2021-09-10 11:22:17 +0200

    Fix confirmation text

--- packages/proposal-components/src/components/VoteAnimatedModal.tsx
+++ packages/proposal-components/src/components/VoteAnimatedModal.tsx
@@ -22,7 +22,7 @@ export function VoteAnimatedModal({
 }: VoteAnimatedModalProps) {
   return (
     <VoteConfirm>
-      <ConfirmText>Your vote for this proposal has been cast!</ConfirmText>
+      <ConfirmText>Your vote {selectedVote === 0 ? 'against' : 'for'} this proposal has been cast!</ConfirmText>
       <VoteChart
         votesFor={votesFor}
         votesAgainst={votesAgainst}
